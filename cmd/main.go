7c378a83fc65fb16898c5786f38c9f52efc5fd72
max-laptop <dolphinikk@gmail.com>
2021-04-08 23:25:07 +0700

    ww

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 
 	"github.com/caarlos0/env"
 	"github.com/keRin7/nexus-manager/nexusmanager"
+	"github.com/keRin7/nexus-manager/pkg/handler"
+	"github.com/keRin7/nexus-manager/pkg/webserver"
 	"github.com/sirupsen/logrus"
 )
 
@@ -21,20 +23,23 @@ func main() {
 	nexus := nexusmanager.New(config)
 	//nexus.List()
 	fmt.Println()
-	tags := nexus.ListTagsByImage("coolrocket/vnikay-dbupdate")
+	//tags := nexus.ListTagsByImage("coolrocket/vnikay-dbupdate")
 
-	for _, v := range tags {
-		//		headers := nexus.GetImageSHA("coolrocket/vnikay", v)
-		nexus.GetDataV1("coolrocket/vnikay-dbupdate", v)
-		size := nexus.GetSize("coolrocket/vnikay-dbupdate", v)
-		fmt.Println(v, size/1024/1024)
-		//		fmt.Println(v, headers["Last-Modified"], headers["Date"])
-	}
+	//for _, v := range tags {
+	//		headers := nexus.GetImageSHA("coolrocket/vnikay", v)
+	//	nexus.GetDataV1("coolrocket/vnikay-dbupdate", v)
+	//	size := nexus.GetSize("coolrocket/vnikay-dbupdate", v)
+	//	fmt.Println(v, size/1024/1024)
+	//		fmt.Println(v, headers["Last-Modified"], headers["Date"])
+	//}
 
-	fmt.Println()
+	//fmt.Println()
 	//nexus.ImageManifest("coolrocket/vnikay", "f9a57d6c4a04448ce2e0f6feb5a61945a02fbd29")
-	fmt.Println()
+	//fmt.Println()
+	handlers := handler.NewHandler(nexus)
 
+	srv := new(webserver.Server)
+	srv.Run("8080", handlers.InitRoutes())
 	//dbcounter := counter.New(config)
 	//dbcounter.Init()
 	//go dbcounter.TablesLenght(ctx)
--- pkg/webserver/webserver.go
+++ pkg/webserver/webserver.go
@@ -1,4 +1,4 @@
-package httpserver
+package webserver
 
 import (
 	"context"
